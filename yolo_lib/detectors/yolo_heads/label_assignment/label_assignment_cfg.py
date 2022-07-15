38a58d57de293c38b4f8f92a65d448fe279b7b01
Markus Tiller <markustiller11@gmail.com>
2022-07-15 09:38:28 +0200

    Updated IoU and DIoU assignment loss configs

--- yolo_lib/detectors/yolo_heads/label_assignment/label_assignment_cfg.py
+++ yolo_lib/detectors/yolo_heads/label_assignment/label_assignment_cfg.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import Optional
+from yolo_lib.detectors.yolo_heads.label_assignment.similarity_metrics.diou_similarity_metric import DIoUPotoMatchloss
 from yolo_lib.detectors.yolo_heads.label_assignment.spatial_prior.spatial_prior_base import SpatialPrior
 from yolo_lib.detectors.yolo_heads.label_assignment.spatial_prior.circular_spatial_prior import CircularSpatialPrior
 from yolo_lib.detectors.yolo_heads.label_assignment.spatial_prior.non_overlapping_spatial_prior import NonOverlappingSpatialPrior
@@ -45,13 +46,29 @@ class IoUBasedOverlappingAssignmentLossCfg(AssignmentLossCfg):
     yx_match_threshold: float
     flat_prior: bool
 
-    alpha: float = 0.8
+    matchloss_objectness_weight: float
+    matchloss_box_weight: float
 
     def get_spatial_prior_fn(self, yx_multiplier: float) -> SpatialPrior:
         return CircularSpatialPrior(self.flat_prior, self.yx_match_threshold)
 
     def get_similarity_metric_fn(self, yx_multiplier: float, num_anchors: int) -> SimilarityMetric:
-        return IoUPotoMatchloss(num_anchors, self.alpha)
+        return IoUPotoMatchloss(num_anchors, self.matchloss_objectness_weight, self.matchloss_box_weight)
+
+
+@dataclass
+class DIoUBasedOverlappingAssignmentLossCfg(AssignmentLossCfg):
+    yx_match_threshold: float
+    flat_prior: bool
+
+    matchloss_objectness_weight: float
+    matchloss_box_weight: float
+
+    def get_spatial_prior_fn(self, yx_multiplier: float) -> SpatialPrior:
+        return CircularSpatialPrior(self.flat_prior, self.yx_match_threshold)
+
+    def get_similarity_metric_fn(self, yx_multiplier: float, num_anchors: int) -> SimilarityMetric:
+        return DIoUPotoMatchloss(num_anchors, self.matchloss_objectness_weight, self.matchloss_box_weight)
 
 
 @dataclass
