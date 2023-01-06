5f210acaef394ebbcc529e48d5c66a1e6fbb757c
Markus Tiller <markustiller11@gmail.com>
2023-01-06 13:06:23 +0100

    Improved default performance metrics

--- yolo_lib/performance_metrics/__init__.py
+++ yolo_lib/performance_metrics/__init__.py
@@ -6,21 +6,30 @@ from .iou_ap import IoUAP, MeanIoUAP
 from .linear_sum_assignment_performance import LinSumPerformance
 from .regression_metrics import CenterDistaneMetric, RegressionMetric, RotationMetric, CenteredIoUMetric, SubclassificationMetrics
 
-def get_default_performance_metrics() -> BasePerformanceMetric:
-    iou_steps = [
-        0.50, 0.55,
-        0.60, 0.65,
-        0.70, 0.75,
-        0.80, 0.85,
-        0.90, 0.95
-    ]
-    return ComposedPerformanceMetrics({
-        "IoU-AP": MeanIoUAP(100, iou_steps),
+def get_default_performance_metrics(
+    include_iou_ap: bool=False,
+    dap_distance_threshold: float=50.0,
+    dap_max_detections_per_tile: int=100,
+    num_classes: int=0,
+) -> BasePerformanceMetric:
+    cfg_dict = {
         "Distance-AP": DistanceAP(
-            distance_threshold=50.0,
-            max_detections=100,
-            regression_metrics=[CenteredIoUMetric(), RotationMetric(), CenterDistaneMetric()],
+            distance_threshold=dap_distance_threshold,
+            max_detections=dap_max_detections_per_tile,
+            regression_metrics=[CenteredIoUMetric(), RotationMetric(), CenterDistaneMetric(), SubclassificationMetrics(num_classes)],
             include_f2=True
         )
-    })
+    }
+
+    if include_iou_ap:
+        iou_steps = [
+            0.50, 0.55,
+            0.60, 0.65,
+            0.70, 0.75,
+            0.80, 0.85,
+            0.90, 0.95
+        ]
+        cfg_dict["Iou-AP"] = MeanIoUAP(100, iou_steps)
+
+    return ComposedPerformanceMetrics(cfg_dict)
 
